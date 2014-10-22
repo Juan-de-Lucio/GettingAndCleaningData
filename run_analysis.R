61bbc7fa562bf6b96f38182f9f1f2ac70f2ab9dd
Juan-de-Lucio <juanjdlf@gmail.com>
2014-10-22 23:37:53 +0200

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,51 @@
+setwd("C:/Users/usuario/Desktop/Juan/GetCleaningData/UCI_HAR_Dataset")
+
+# 1. Merges the training and the test sets to create one data set.
+
+tmp1 <- read.table("train/X_train.txt")
+tmp2 <- read.table("test/X_test.txt")
+X <- rbind(tmp1, tmp2)
+tmp1 <- read.table("train/subject_train.txt")
+tmp2 <- read.table("test/subject_test.txt")
+Subject <- rbind(tmp1, tmp2)
+tmp1 <- read.table("train/y_train.txt")
+tmp2 <- read.table("test/y_test.txt")
+Y <- rbind(tmp1, tmp2)
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+features <- read.table("features.txt")
+indices_of_good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
+X <- X[, indices_of_good_features]
+names(X) <- features[indices_of_good_features, 2]
+names(X) <- gsub("\\(|\\)", "", names(X))
+names(X) <- tolower(names(X))
+
+# 3. Uses descriptive activity names to name the activities in the data set.
+activities <- read.table("activity_labels.txt")
+activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
+Y[,1] = activities[Y[,1], 2]
+names(Y) <- "activity"
+
+# 4. Appropriately labels the data set with descriptive activity names.
+names(Subject) <- "subject"
+cleaned <- cbind(Subject, Y, X)
+write.table(cleaned, "merged_clean_data.txt")
+
+# 5. Creates a 2nd, independent tidy data set with the average of each variable for each activity and each subject.
+uniqueSubjects = unique(Subject)[,1]
+numSubjects = length(unique(Subject)[,1])
+numActivities = length(activities[,1])
+numCols = dim(cleaned)[2]
+result = cleaned[1:(numSubjects*numActivities), ]
+row = 1
+for (s in 1:numSubjects) {
+for (a in 1:numActivities) {
+result[row, 1] = uniqueSubjects[s]
+result[row, 2] = activities[a, 2]
+tmp <- cleaned[cleaned$subject==s & cleaned$activity==activities[a, 2], ]
+result[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
+row = row+1
+}
+}
+
+write.table(result, "tidy_data.txt")
